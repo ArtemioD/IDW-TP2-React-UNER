b3d2225affea689e6cf46acd464a6261204f77cb
MariaDerkacheva <derkachevamaria@gmail.com>
2024-05-31 16:50:27 -0300

    realice About y footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import Header from "./components/Header";
+
+import Footer from "./components/footer/footer.js";
+import NavBar from "./components/navbar/NavBar.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Contacto from "./pages/Contacto.js";
 
 function App() {
   return (
-    <Header />
-  )
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
