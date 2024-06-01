6e1d3eb2706e100d491f8df9495889079bc9bee1
NicoOjeda <nico_ojeda86@hotmail.com>
2024-06-01 16:31:01 -0300

    se agrega ruta addprop a app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 import Contacto from "./pages/Contacto.js";
+import AddProp from "./pages/AddProp.js"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route index path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contacto" element={<Contacto />} />
+          <Route path="/addpropiedad" element={<AddProp />} />
         </Routes>
         <Footer/>
       </BrowserRouter>
