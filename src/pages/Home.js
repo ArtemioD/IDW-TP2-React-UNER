73f9230a919c4d57ca577fea06e1c10042122253
MariaDerkacheva <derkachevamaria@gmail.com>
2024-06-03 18:21:32 -0300

    Titulo en el home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import Cards from "../components/cards/Cards"
-
+import titulo from "../img/titulo.png";
+import "./Home.css"
 
 const Home = () => {
     return (
-        <div>
+        <div className="contenedor">
+            <div className="titulo">
+                <img src={titulo} alt="Titulo" />
+            </div>
             <Cards/>
         </div>
         
