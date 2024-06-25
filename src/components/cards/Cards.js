2ea9698b6ec065e715f5a83fb4ac0917f886b056
artemio <artemio.derkachev@gmail.com>
2024-06-24 21:29:36 -0300

    funcionalidad del buscador por titulo

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,93 +1,108 @@
 import { useEffect, useState } from 'react'
 import "./Cards.css"
-import { Link as LinkRouter} from 'react-router-dom'
+import { Link as LinkRouter } from 'react-router-dom'
 
 
-function Cards() {
+function Cards({ valorBuscado }) {
 
-        const [hoteles, setHoteles] = useState([])
-        const [imagenes, setImagenes] = useState([])
 
-    
-        useEffect(()=>{
-            getAlojamientos()
-            getImagenes()
-        },[])
-        
-        const getAlojamientos = async () => {
-            try {
-                const response = await fetch("http://localhost:3001/alojamiento/getAlojamientos", {
-                    method: 'GET',
-                    headers: {
-                        'Content-type': 'application/json',
-                    }
-                })
-                if (response.ok) {
-                    const data = await response.json()
-                    setHoteles(data)
-                } else {
-                    console.error("ERROR: al obtener alojamientos", response.body)
+    const [hoteles, setHoteles] = useState([])
+    const [imagenes, setImagenes] = useState([])
+
+    useEffect(() => {
+        getAlojamientos()
+        getImagenes()
+    }, [])
+
+    const getAlojamientos = async () => {
+        try {
+            const response = await fetch("http://localhost:3001/alojamiento/getAlojamientos", {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
                 }
-            } catch (error) {
-                console.log("ERROR: ", error)
+            })
+            if (response.ok) {
+                const data = await response.json()
+                setHoteles(data)
+            } else {
+                console.error("ERROR: al obtener alojamientos", response.body)
             }
+        } catch (error) {
+            console.log("ERROR: ", error)
         }
-    
-        const getImagenes = async (id) => {
-            try {
-                const response = await fetch(`http://localhost:3001/imagen/getAllImagenes?idAlojamiento=${id}`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-type': 'application/json',
-                    }
-                })
-                if (response.ok) {
-                    const data = await response.json()
-                    setImagenes(data)
-                } else {
-                    console.error("ERROR: al obteber imagenes", response.body)
+    }
+
+    const getImagenes = async (id) => {
+        try {
+            const response = await fetch(`http://localhost:3001/imagen/getAllImagenes?idAlojamiento=${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
                 }
-            } catch (error) {
-                console.log("ERROR: ", error)
+            })
+            if (response.ok) {
+                const data = await response.json()
+                setImagenes(data)
+            } else {
+                console.error("ERROR: al obteber imagenes", response.body)
             }
+        } catch (error) {
+            console.log("ERROR: ", error)
         }
+    }
 
-        console.log(hoteles);
-        console.log(imagenes);
+    console.log(hoteles);
+    console.log(imagenes);
 
-  return (
-    <div className="containers">
-        {
-            
-            hoteles.map((alojamiento=>(
-                <div className="cards" key={alojamiento.id}>
-                    <LinkRouter to={`/details/${alojamiento.idAlojamiento}`} className="text-decoration-none text-dark textCard">
-                    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel" data-bs-interval="9000">
-                        <div className="carousel-inner">
-                            
-                            {
-                                
-                                imagenes.map((imag)=>(
-                                    <div className="carousel-item active imgCard">
-                                        <img src={imag.RutaArchivo} className="d-block w-100 borderImg" alt={imag.idImagen}/>
-                                    </div>
-                                    
-                                ))
-                            }
-                            
-                        </div>
+    const alojamientoEncontrado = hoteles.find(aloj => aloj.Titulo.toLowerCase() === valorBuscado.toLowerCase());
+
+    return (
+        <div className="containers">
+            {valorBuscado === "" ? (
+                hoteles.map((alojamiento => (
+                    <div className="cards" key={alojamiento.id}>
+                        <LinkRouter to={`/details/${alojamiento.idAlojamiento}`} className="text-decoration-none text-dark textCard">
+                            <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel" data-bs-interval="9000">
+                                <div className="carousel-inner">
+
+                                    {
+
+                                        imagenes.map((imag) => (
+                                            <div className="carousel-item active imgCard">
+                                                <img src={imag.RutaArchivo} className="d-block w-100 borderImg" alt={imag.idImagen} />
+                                            </div>
+
+                                        ))
+                                    }
+
+                                </div>
+                            </div>
+                            <h3 className="h3Card">{alojamiento.Titulo}</h3>
+                            <p className="p1Card">{alojamiento.Descripcion}</p>
+                            <p className="p2Card">{alojamiento.PrecioPorDia}</p>
+                        </LinkRouter>
                     </div>
-                        <h3 className="h3Card">{alojamiento.Titulo}</h3>
-                        <p className="p1Card">{alojamiento.Descripcion}</p>
-                        <p className="p2Card">{alojamiento.PrecioPorDia}</p>
-                    </LinkRouter>
-                </div>
-            )
-            ))
-        }
-        
-    </div>
-  )
+                )
+                ))) :
+                (
+                    alojamientoEncontrado ? (
+                        <div className="cards">
+                            <LinkRouter to={`/details/${alojamientoEncontrado.idAlojamiento}`} className="text-decoration-none text-dark textCard">
+                                <h3 className="h3Card">{alojamientoEncontrado.Titulo}</h3>
+                                <p className="p1Card">{alojamientoEncontrado.Descripcion}</p>
+                                <p className="p2Card">{alojamientoEncontrado.PrecioPorDia}</p>
+                            </LinkRouter>
+                        </div>
+                    ) : (
+                        <div>No se encontró alojamiento con el título "{valorBuscado}"</div>
+                    )
+                
+                )
+            }
+
+        </div>
+    )
 }
 
 export default Cards
